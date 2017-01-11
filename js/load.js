f20565ed1af2c5284b6097864f0b406ecbbe2a8d
liadoc <cjlee300@gmail.com>
2017-01-11 03:49:59 -0500

    Toolbar Mobile Fix Attempt

--- js/load.js
+++ js/load.js
@@ -17,9 +17,10 @@ $(document).ready(function(){
             return true;
         } else {
             link.addClass('hover');
-            $('a.taphover').not(this).removeClass('hover').closest("td").css("background", "#545454");
+            $('a.taphover').not(this).removeClass('hover');
+            $('a.taphover').not(this).closest("td").css("background", "#545454")
             e.preventDefault();
-            var td = $(this).closest("td")
+            var td = link.closest("td")
             td.css("background", "rgb(235, 100, 75)")
             $("#label").stop(false, true).hide(0).css('background-image', 'radial-gradient(at top, rgba(236, 100, 75, 1) 10%, rgba(255, 255, 255, 0) 20%)').fadeIn(200);
             $("#actualLabel").text(td.attr('id')).stop(false, true).fadeIn(200);
