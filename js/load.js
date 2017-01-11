2783556ad9fe3de437f90ab95fe3a4fbf602680c
liadoc <cjlee300@gmail.com>
2017-01-11 18:23:26 -0500

    About Page

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $("#intro").hide(0).delay(500).fadeIn(1000)
+    $("#intro, #about").hide(0).delay(500).fadeIn(1000)
     $("#nav, #label").hide(0).delay(1500).fadeIn(1000)
-    $("table tr td").each(function(i, obj) {
+    $(".button").each(function(i, obj) {
         $(obj).hover(function() {
             $("#label").stop(false, true).hide(0).css('background-image', 'radial-gradient(at top, rgba(236, 100, 75, 1) 10%, rgba(255, 255, 255, 0) 20%)').fadeIn(200);
             $("#actualLabel").text($(obj).attr('id')).stop(false, true).fadeIn(200);
