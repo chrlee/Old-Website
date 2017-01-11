8bfb102030fc381523bfdf64f5e8dfa53acaf944
liadoc <cjlee300@gmail.com>
2017-01-11 03:43:50 -0500

    Contact Page and Toolbar Mobile Optimization

--- js/load.js
+++ js/load.js
@@ -10,4 +10,20 @@ $(document).ready(function(){
             $("#actualLabel").stop(false, true).fadeOut(200)
         })
     });
+    $('a.taphover').on('touchstart', function (e) {
+        'use strict'; //satisfy code inspectors
+        var link = $(this); //preselect the link
+        if (link.hasClass('hover')) {
+            return true;
+        } else {
+            link.addClass('hover');
+            $('a.taphover').not(this).removeClass('hover').closest("td").css("background", "#545454");
+            e.preventDefault();
+            var td = $(this).closest("td")
+            td.css("background", "rgb(235, 100, 75)")
+            $("#label").stop(false, true).hide(0).css('background-image', 'radial-gradient(at top, rgba(236, 100, 75, 1) 10%, rgba(255, 255, 255, 0) 20%)').fadeIn(200);
+            $("#actualLabel").text(td.attr('id')).stop(false, true).fadeIn(200);
+            return false; //extra, and to make sure the function has consistent return points
+        }
+    });
 });
